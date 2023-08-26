93dc890d421a18dcbb0ec21d3407834c62327644
DavidDSantanaks <davidfjobs@gmail.com>
2023-08-26 08:34:47 -0300

    Arrumando detalhes de escrita apenas, codigo mantem o mesmo

---
+++ src/main/java/com/davidSantana/todosimple/models/Task.java
@@ -0,0 +1,77 @@
+package com.davidSantana.todosimple.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Entity //A classe funciona como uma tabela de banco de dados
+@Table(name = Task.TABLE_NAME) //Nome da tabela no banco de dados
+public class Task {
+    
+    public static final String TABLE_NAME = "task"; //Constante para força o nome da tabela
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name =  "id", unique = true)
+    private Long id;
+
+    @ManyToOne//Varias coisas de um unico usuario da classe
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user; //A tasnk pertence a um usuario
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotBlank
+    @Size(min = 1 , max = 255)
+    private String description;
+
+
+    //Construtor
+
+    public Task() {
+    }
+
+
+    public Task(Long id, User user, String description) {
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+
+
+    //Get e set
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+}
--- src/main/java/com/davidSantana/todosimple/models/User.java
+++ src/main/java/com/davidSantana/todosimple/models/User.java
@@ -6,12 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 //INFORMA PARA O BANCO QUE ESTA CLASSE É UMA TABELA DO BANCO DE DADOS
@@ -31,7 +38,7 @@ public class User {
     @Id //Informa para o banco de dados que esse campo é de ID
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Informa para o banco de dados que é para auto incrementar os ID 
     @Column(name = "id", unique = true) //Força o nome da coluna no banco
-    private Integer id;
+    private Long id;
 
     @Column(name = "userName", length = 100, nullable = false, unique = true ) //Força o nome na coluna, define o tamanho, pode ser nulo e que a informação é unica.
     @NotNull(groups = createUser.class) //NÃO PERMITE VALOR NULO
@@ -39,30 +46,37 @@ public class User {
     @Size(min = 2, max = 100) //Define o tamanho do campo
     private String userName;
 
+
     @NotBlank(groups = createUser.class) //Funciona como o notNull e notEmpty ao mesmo tempo.
     @Size(groups = {createUser.class, UpdateUser.class}, min = 8, max = 60) //Define o tamanho do campo
+    @JsonProperty(access = Access.WRITE_ONLY)//Não retorna a senha 
     @Column(name = "password", length = 60, nullable = false)
     private String password;
 
-    public User() {
-    }
 
-    //private List<Task> task = new ArrayList<Task>;
+    @OneToMany(mappedBy = "user") //Um usuario para varias tarefas
+    private List<Task> task = new ArrayList<Task>();
+    
 
 
     //Contrutor
-    public User(Integer id, String userName, String password) {
+    public User(Long id, String userName, String password) {
         this.id = id;
         this.userName = userName;
         this.password = password;
     }
+
+    public User() {
+    }
+
     
     //Getter and Setters
-    public Integer getId() {
+
+    public Long getId() {
         return this.id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -81,6 +95,17 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+
+    public List<Task> getTask() {
+        return this.task;
+    }
+
+    public void setTask(List<Task> task) {
+        this.task = task;
+    }
+
+    
 
     //SEM EQUAL E HASH SE DER BO SÓ VOLTA NA AULA 5
     
